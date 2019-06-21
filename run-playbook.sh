6387e6a4c24be46a62056560f6483caeaa409952
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2019-06-21 18:01:39 -0300

    soh pra bkp

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,18 +1,18 @@
 
 echo " ===> Initial Configuration"
-ansible-playbook -i hosts.yml initial.yml
+ansible-playbook -v -i hosts.yml initial.yml
 
 echo " ===> Install kubernetes dependencies"
-ansible-playbook -i hosts.yml kube-dependencies.yml
+ansible-playbook -v -i hosts.yml kube-dependencies.yml
 
 echo " ===> Configuring master node"
-ansible-playbook -i hosts.yml master.yml
+ansible-playbook -v -i hosts.yml master.yml
 
 echo " ===> Configuring workers nodes"
-ansible-playbook -i hosts.yml workers.yml
+ansible-playbook -v -i hosts.yml workers.yml
 
 echo " ===> Install Helm"
-ansible-playbook -i hosts.yml helm.yml
+ansible-playbook -v -i hosts.yml helm.yml
 
 echo " ===> Installing and configuring some aditional features"
-ansible-playbook -i hosts.yml pods.yml
+ansible-playbook -v -i hosts.yml pods.yml
